219cdd278809269210980ef29a47779f3ff6aaaf
ponyspy <desade4me@gmail.com>
2019-04-20 17:08:10 +0300

    fix

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,7 @@
 $(function() {
 	// var buttons = {40: 'down', 38: 'up', 37: 'left', 39: 'right'};
+	var swipers = [];
+	var currentV = 0;
 
 	var swiperV = new Swiper('.swiper-container-v', {
 		slidesPerView: 1,
@@ -10,32 +12,46 @@ $(function() {
 		// keyboardControl: true,
 		on: {
 			slideChange: function() {
+				currentV = this.realIndex;
 				$('.navigate-block').removeClass('active').eq(this.realIndex).addClass('active');
 			}
 		}
 	});
 
-	var swiperH = new Swiper('.swiper-container-h', {
-		slidesPerView: 1,
-		direction: 'horizontal',
-		// spaceBetween: 30,
-		// autoHeight: true,
-		// initialSlide: 1,
-		// centeredSlides: true,
-		// keyboardControl: true,
-		loop: true,
-		runCallbacksOnInit: false,
+	$('.swiper-container-h').toArray().forEach(function(swiper) {
+		var swiperH = new Swiper(swiper, {
+			slidesPerView: 1,
+			direction: 'horizontal',
+			// spaceBetween: 30,
+			// autoHeight: true,
+			// initialSlide: 1,
+			// centeredSlides: true,
+			// keyboardControl: true,
+			loop: true,
+			runCallbacksOnInit: false,
 
-		// preloadImages: false,
-		// lazyLoading: true,
-		// lazyLoadingInPrevNext: true,
-		// lazyLoadingInPrevNextAmount: 2,
+			// preloadImages: false,
+			// lazyLoading: true,
+			// lazyLoadingInPrevNext: true,
+			// lazyLoadingInPrevNextAmount: 2,
 
+		});
+
+		swipers.push(swiperH);
 	});
 
-	$(document).on('keydown', function(e) {
+	$(document).on('keyup', function(e) {
+
 		if (e.which == 40) {
 			swiperV.slideNext();
+		}
+
+		if (e.which == 37) {
+			swipers[currentV].slidePrev();
+		}
+
+		if (e.which == 39) {
+			swipers[currentV].slideNext();
 		}
 	});
 
